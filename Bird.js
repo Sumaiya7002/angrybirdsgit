40e1204e2b22b3cd473cd1a4336f916bd05ffaf2
sumaiya7002 <sumaiyaib1@gmail.com>
2021-01-09 19:56:32 +0530

    Sumaiya

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeimg = loadImage("sprites/smoke.png")
+    this.trag = [];
   }
 
   display() {
@@ -9,5 +11,15 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.velocity.x>10&&this.body.position.x>200){
+    
+      var position = [this.body.position.x,this.body.position.y]
+    this.trag.push(position)
+    }
+    
+    for(var i = 0; i<this.trag.length; i++){
+      image(this.smokeimg,this.trag[i][0],this.trag[i][1])
+
+    }
   }
 }
